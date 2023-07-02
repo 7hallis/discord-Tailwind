7cabda6aa8407608fefca96582ae6de056679a70
7hallis <thallisffc@hotmail.com>
2023-07-02 00:02:19 -0300

    discord tailwind css

---
+++ src/app/components/LogoPlus.tsx
@@ -0,0 +1,10 @@
+export function LogoPlus() {
+    return (
+        <div>
+            <svg width="27" height="22" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 0H7V7H0V9H7V16H9V9H16V7H9V0Z" fill="#3BA55D"/>
+            </svg>
+        </div>
+      
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,23 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'bgdc1' : '#8E9297',
+      'bgdc2' : '#C7C9CB',
+      'dcblue' : '#5865F2',
+      'dcblue100':'#6865F2',
+      'dcgreen' : '#3BA55D',
+      'dcred' : '#ED4245',
+      'dcfundo' : '#2F3136',
+      'dcfundo2' : '#42454A',
+      'traco' : '#2D2F32',
+      'dcfundo3': '#202225',
+      'textColor' : '#8E9297',
+      'textWhite' : '#fff',
+      'textHover' : '#393C43',
+      'fundo3' : '#36393F'
+    }
   },
   plugins: [],
+  
 }
